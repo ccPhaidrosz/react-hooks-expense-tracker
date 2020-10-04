fd8ff24614e256975459546dcc386352ede773df
ccPhaidrosz <arvylphoenix@gmail.com>
2020-10-05 00:06:36 +0200

    Updates

---
+++ precache-manifest.8419126b8c7ee07f931b5139ff782a3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90644b43df56fad36962c66c06f2c250",
+    "url": "/react-hooks-expense-tracker/index.html"
+  },
+  {
+    "revision": "648dc98123ae68fb7285",
+    "url": "/react-hooks-expense-tracker/static/css/main.ee45eb7d.chunk.css"
+  },
+  {
+    "revision": "a3886eb48eeb822dea54",
+    "url": "/react-hooks-expense-tracker/static/js/2.abb637fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hooks-expense-tracker/static/js/2.abb637fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "648dc98123ae68fb7285",
+    "url": "/react-hooks-expense-tracker/static/js/main.f11e2bb4.chunk.js"
+  },
+  {
+    "revision": "b9505b88fa735bf32071",
+    "url": "/react-hooks-expense-tracker/static/js/runtime-main.d5f009cb.js"
+  }
+]);
